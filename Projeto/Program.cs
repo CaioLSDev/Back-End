7a75d43747ade85b853b5a1559659deb592a2c4e
CaioLSDev <107144210+CaioLSDev@users.noreply.github.com>
2022-12-08 13:05:09 -0300

    Delete Projeto directory

--- Projeto/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Projeto.obj.Classes;
-
-PessoaFisica pessoaF01 = new PessoaFisica();
-
-pessoaF01.nome = "Moira";
-pessoaF01.cpf = "00011100010";
-
-System.Console.WriteLine("O CPF de " +pessoaF01.nome+ " é " +pessoaF01.cpf);
